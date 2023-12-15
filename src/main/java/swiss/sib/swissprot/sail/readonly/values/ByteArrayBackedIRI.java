33db7abbd379473fc6b1000dee986d17d93620d5
Jerven Bolleman <jerven.bolleman@sib.swiss>
2023-12-15 10:12:05 +0100

    Clean up dependency on not commited code.

--- src/main/java/swiss/sib/swissprot/sail/readonly/values/ByteArrayBackedIRI.java
+++ src/main/java/swiss/sib/swissprot/sail/readonly/values/ByteArrayBackedIRI.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Eclipse RDF4J contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *******************************************************************************/
 package swiss.sib.swissprot.sail.readonly.values;
 
 import java.nio.charset.StandardCharsets;
@@ -5,9 +15,14 @@ import java.util.Arrays;
 
 import org.eclipse.rdf4j.model.IRI;
 
-import swiss.sib.swissprot.sail.readonly.datastructures.list.IRISection;
-
+/**
+ * An IRI implementation backed by a single byte array of UTF_8 encoded
+ * characters. 
+ */
 public record ByteArrayBackedIRI(byte[] backing) implements IRI {
+	private static final byte HASH = (byte) '#';
+	private static final byte SLASH = (byte) '/';
+	private static final byte COLON = (byte) ':';
 
 	@Override
 	public String stringValue() {
@@ -16,12 +31,49 @@ public record ByteArrayBackedIRI(byte[] backing) implements IRI {
 
 	@Override
 	public String getNamespace() {
-		return new String(backing, 0, IRISection.getLocalNameIndex(backing), StandardCharsets.UTF_8);
+		return new String(backing, 0, getLocalNameIndex(), StandardCharsets.UTF_8);
 	}
 
 	@Override
 	public String getLocalName() {
-		return new String(backing, IRISection.getLocalNameIndex(backing), backing.length, StandardCharsets.UTF_8);
+		return new String(backing, getLocalNameIndex(), backing.length, StandardCharsets.UTF_8);
+	}
+
+	// @See URIUtil class for the source logic
+	private int getLocalNameIndex() {
+		int separatorIdx = indexOf(backing, HASH);
+
+		if (separatorIdx < 0) {
+			separatorIdx = lastIndexOf(backing, SLASH);
+		}
+
+		if (separatorIdx < 0) {
+			separatorIdx = lastIndexOf(backing, COLON);
+		}
+
+		if (separatorIdx < 0) {
+			throw new IllegalArgumentException("No separator character founds in URI: " + stringValue());
+		}
+
+		return separatorIdx + 1;
+	}
+
+	private int lastIndexOf(byte[] backing2, byte c) {
+		for (int i = backing2.length; i >= 0; i--) {
+			if (backing2[i] == c) {
+				return c;
+			}
+		}
+		return -1;
+	}
+
+	private int indexOf(byte[] backing2, byte c) {
+		for (int i = 0; i < backing2.length; i++) {
+			if (backing2[i] == c) {
+				return c;
+			}
+		}
+		return -1;
 	}
 
 	@Override
